8d102c30ffeb213b764bb69bc89851c6b229192c
Jackson Gordon <jag04460@ucmo.edu>
2023-03-15 15:56:24 -0500

    testing commit

--- Beehive_FrontEnd-Master/app/src/main/java/com/example/themainbuzzlogin2/DBHelper.java
+++ Beehive_FrontEnd-Master/app/src/main/java/com/example/themainbuzzlogin2/DBHelper.java
@@ -24,6 +24,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists users");
+        //testing
     }
 
     public boolean insertData(String username, String password, String name, int age){
